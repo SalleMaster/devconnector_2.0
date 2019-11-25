1622ce4f71d111cf35610566672c61ac38c920b9
Sasa Radovanovic <salle90ar@gmail.com>
2019-11-25 23:38:36 +0100

    Delete Education, Experience & Account

--- client/src/components/profile-form/AddEducation.js
+++ client/src/components/profile-form/AddEducation.js
@@ -91,7 +91,7 @@ const AddEducation = ({ addEducation, history }) => {
                 toggleDisabled(!toDateDisabled);
               }}
             />{' '}
-            Current Job
+            Current
           </p>
         </div>
         <div className='form-group'>
